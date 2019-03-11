7341239218896a0cc10a26aa945c1c0634ba2cb5
Melvin Hernandez <melher39@gmail.com>
2019-03-11 18:15:01 -0500

    added server.js with a quick setup

---
+++ server.js
@@ -0,0 +1,27 @@
+// modeled after week 20 activity 7 server.js file
+// our dependencies
+import express from "express";
+import path from "path";
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// static assets for heroku deployment
+if(process.end.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// API routes here
+
+// Every other request will be sent to the React app
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, ".client/build/index.html"));
+});
+
+// start our port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
+});
