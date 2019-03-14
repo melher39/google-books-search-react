d6b08a48c49c3859dec45447a0c9a61b8c8aff6c
Melvin Hernandez <melher39@gmail.com>
2019-03-14 12:10:59 -0500

    added index.js to routes folder to redirect request to either the client or api

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// dependencies
+// based off week 20 activity 11 
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API Routes
+// if API routes are the target, then redirect that to the proper route
+router.use("/api", apiRoutes);
+
+// Every other request will be sent to the React app, if no API routes are hit
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // modeled after week 20 activity 7 server.js file
+// activity 11
 // our dependencies
 const express = require("express");
-const path = require("path");
+// const path = require("path");
+const routes = require("./routes");
 const mongoose = require("mongoose");
 
 // const routes = require("./routes");
@@ -17,13 +19,8 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-// API routes here
-
-// Every other request will be sent to the React app
-// not sure if this will be necessary
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, ".client/build/index.html"));
-});
+// Add routes, both API and view
+app.use(routes);
 
 // If deployed, use the deployed database. Otherwise use the local googleBooks database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googleBooks";
