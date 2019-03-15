fd5fe369b866c0da7d4edf6fbc541ea18ae75e2a
Melvin Hernandez <melher39@gmail.com>
2019-03-15 11:06:01 -0500

    added content to googleSearch.js which will be responsible for calling the google books api

---
+++ routes/api/books.js
@@ -0,0 +1,10 @@
+// based off week 20 activity 11
+// dependencies
+const router = require("express").Router();
+// const booksController = require("../../controllers");
+
+// routes that match "/api/books"
+// below taken from assignment instructions
+// /api/books (get) - Should return all saved books as JSON.
+// /api/books (post) - Will be used to save a new book to the database.
+router.route("/")
---
+++ routes/api/googleSearch.js
@@ -0,0 +1,20 @@
+// this file will be responsible for connecting to the Google Books API
+// modeled after week 20 activity 8 
+// dependencies
+const axios = require("axios");
+const router = require("express").Router();
+
+// once we hit this route /googlesearch
+router.get("/googlesearch", (req, res) => {
+    // make the API call to the google books api
+axios
+    .get("https://www.googleapis.com/books/v1/volumes", {params: req.query})
+    // retrieve the data we actually want, which is in the `items` array
+    .then(results => results.data.items)
+    // thrn convert this data into a json file ready for us to manipulate
+    .then(results => res.json(results))
+    // and if there is an error, catch it and return it to us
+    .catch(err => res.status(422).json(err));
+});
+
+module.exports = router;
